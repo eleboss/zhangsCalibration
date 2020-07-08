0138be07761ce749fd8770d1cd4bab5db940f68f
eleboss <1>
2020-07-08 20:23:04 +0800

    sync

---
+++ build/CMakeFiles/zcal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zcal.dir/zhangsCalbration.cpp.o"
+  "zcal.pdb"
+  "zcal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zcal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zhangsCalbration.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>        
+#include <opencv2/core/eigen.hpp>
+
+using namespace std;
+using namespace cv;
+using namespace Eigen;
+
+int main (int argc, char *argv[])
+{
+
+    
+    return 0;
+}
